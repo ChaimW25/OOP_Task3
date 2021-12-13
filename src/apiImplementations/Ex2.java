e82bed3033ee99edbd73e2524294a47051a09c72
ChaimW25 <willingerch@gmail.com>
2021-12-13 18:58:02 +0200

    Revert "Delete Ex2.java"
    This reverts commit ba9ccbc502a6dcc976ed25a6b2bca5402521dd05.

---
+++ src/apiImplementations/Ex2.java
@@ -0,0 +1,49 @@
+package apiImplementations;
+
+import api.DirectedWeightedGraph;
+import api.DirectedWeightedGraphAlgorithms;
+import gui.Frame;
+import myGui.gui;
+
+/**
+ * This class is the main class for Ex2 - your implementation will be tested using this class.
+ */
+public class Ex2 {
+    /**
+     * This static function will be used to test your implementation
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     * @return
+     */
+    public static DirectedWeightedGraph getGrapg(String json_file) {
+        DirectedWeightedGraphAlgorithms graphAlgo = new DWGAlgo();
+        graphAlgo.load(json_file);
+        return graphAlgo.getGraph();
+    }
+    /**
+     * This static function will be used to test your implementation
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     * @return
+     */
+    public static DirectedWeightedGraphAlgorithms getGrapgAlgo(String json_file) {
+        DirectedWeightedGraphAlgorithms graphAlgo = new DWGAlgo();
+        graphAlgo.load(json_file);
+        return graphAlgo;
+    }
+    /**
+     * This static function will run your GUI using the json fime.
+     * @param json_file - a json file (e.g., G1.json - G3.gson)
+     *
+     */
+    public static void runGUI(String json_file) {
+        DirectedWeightedGraphAlgorithms graphAlgo = getGrapgAlgo(json_file);
+        gui g = new gui(graphAlgo.getGraph());
+        g.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        String path = System.getProperty("user.dir") + "\\data\\";
+        String json_file = "G1.json";
+        runGUI(path + json_file);
+    }
+}
+
